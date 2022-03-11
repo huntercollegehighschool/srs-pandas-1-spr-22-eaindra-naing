79f430bf29980ff8e0f22548e5ab197290192210
Eaindra Naing <79598728+eaindra-naing@users.noreply.github.com>
2022-03-11 17:31:56 +0000

    some progress with pandas

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ We'll be using the pandas (short for panels of data) module to analyze a large d
 1. The conventional way to import the pandas module is below. Note that the "as pd" part is optional (but widely used).
 import pandas as pd
 """
-
+import pandas as pd
 
 """
 2. This folder contains a large .csv (spreadsheet) file named ign.csv. Import the data stored in the csv as a dataframe to Python and store it in a variable. Try printing the data.
 pd.read_csv('<filename/url>')
 """
-
+data = pd.read_csv('ign.csv')
+#print (data)
 
 """
 3. When you try to view the data on the console, you should notice that the row index appears twice. That's because if not specified, pandas will create it's own indices. In fact, when you look at the data, because it's so large, you can't even see the title of the game.
@@ -20,24 +21,27 @@ pd.read_csv('<filename/url>')
 Change the dataframe so that the index are not integers starting from zero, but instead the title of the game. You'll have to go to the .csv file to find in which column the game titles are.
 pd.read_csv('<filename/url>', index_col = <column number>)
 """
-
+specdata = pd.read_csv ('ign.csv', index_col = 2)
+print (specdata)
 
 """
 4. Use <dataframe>.index and print the result to see a list of indices/row names. (Note: the console will not print all of them)
 """
-
+print (specdata.index)
 
 """
 5. Use <dataframe>.columns and print the result to see a list of column names.
 """
-
+print (specdata.columns)
 """
 6. In a pandas dataframe, columns are indexed by column name. For example, reviews['score_phrase'] returns a dataframe that contains only the index (the game title) and the score phrase given to that game.
 
 Create a dataframe that only contains the score for each game. You can save it in a new dataframe variable, but you don't have to. Print that new dataframe.
 """
+#score = pd.read_csv('ign.csv',index_col = 1)
+#print (score)
 
-
+specdata.reviews['score_phrase']
 """
 7. Use the following functions to find and print the indicated values based off score. Use the dataframe you created in #6.
 <dataframe>.max()
@@ -46,7 +50,11 @@ Create a dataframe that only contains the score for each game. You can save it i
 <dataframe>.median()
 <dataframe>.std()  # standard deviation
 """
-
+print (score.max())
+print (score.min())
+print (score.mean())
+print (score.median())
+print (score.std())
 """
 8. pandas has a function that returns a series indicating how many of each value is in a dataframe (<dataframe>.value_counts()). Use that function to find the counts in the platform, score, and release_month columns, and print the results. You will want to create 3 new dataframes that you can use value_counts() on.
 """
