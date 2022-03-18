03a0d7ae91a15d305bfe0ac064077cb5925ff63d
Eaindra Naing <79598728+eaindra-naing@users.noreply.github.com>
2022-03-18 16:33:05 +0000

    Finished the assignment

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ print (specdata.columns)
 
 Create a dataframe that only contains the score for each game. You can save it in a new dataframe variable, but you don't have to. Print that new dataframe.
 """
-#score = pd.read_csv('ign.csv',index_col = 1)
+score = pd.read_csv('ign.csv',index_col = 2)
 #print (score)
 
-specdata.reviews['score_phrase']
+#specdata.reviews['score_phrase']
 """
 7. Use the following functions to find and print the indicated values based off score. Use the dataframe you created in #6.
 <dataframe>.max()
@@ -50,23 +50,32 @@ specdata.reviews['score_phrase']
 <dataframe>.median()
 <dataframe>.std()  # standard deviation
 """
-print (score.max())
-print (score.min())
-print (score.mean())
-print (score.median())
-print (score.std())
+print (score["score"].max())
+print (score["score"].min())
+print (score["score"].mean())
+print (score["score"].median())
+print (score["score"].std())
 """
 8. pandas has a function that returns a series indicating how many of each value is in a dataframe (<dataframe>.value_counts()). Use that function to find the counts in the platform, score, and release_month columns, and print the results. You will want to create 3 new dataframes that you can use value_counts() on.
 """
-
+platform = specdata["platform"]
+print (platform.value_counts())
+score = specdata["score"]
+print (score.value_counts())
+release_month = specdata["release_month"]
+print (release_month.value_counts())
 """
 9. pandas supports Boolean indexing. For example, reviews[reviews['score'] < 1] would return a dataframe containing games that got a score below a 1.
 
 Create 2 new dataframes, one with only games that got scores below 2, and one with only games that got scores of 10. Print both dataframes.
 """
-
+loser = specdata[specdata['score'] < 2]
+winner = specdata[specdata['score'] == 10]
+print (loser)
+print (winner)
 """
 10. Save both the new dataframes you created in #9 to a csv file (it will make it much nicer to look at). Name the files 'trash.csv' and 'perfect.csv'
 <dataframe>.to_csv('<filename>')
 """
-
+loser.to_csv('trash.csv')
+winner.to_csv('perfect.csv')
